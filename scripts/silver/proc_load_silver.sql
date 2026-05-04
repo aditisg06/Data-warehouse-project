5abbc5cdbea2acedb0d416ac9c782b15f78aa158
aditisg06 <124760046+aditisg06@users.noreply.github.com>
2026-05-04 15:37:34 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -24,20 +24,29 @@ BEGIN
 					cst_create_date
 				)
 
-		select cst_id, 
-					cst_key, 
-					TRIM(cst_firstname) AS cst_firstname, 
-					TRIM(cst_lastname) AS cst_lastname, 
-					case when UPPER(TRIM(cst_marital_status))='S' THEN 'Single'
-						 when UPPER(TRIM(cst_marital_status))='M' THEN 'Married'
-						 else 'n/a'
-					end cst_marital_status, 
-					case when UPPER(TRIM(cst_gndr))='F' THEN 'Female'
-						 when UPPER(TRIM(cst_gndr))='M' THEN 'Male'
-						 else 'n/a'
-					end cst_gndr,
-					cst_create_date from(select *, ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC)as flag_last 
-					from bronze.crm_cust_info)t where flag_last=1;
+		SELECT
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+			cst_create_date
+		FROM (
+			SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		) t WHERE flag_last = 1;
 		SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
