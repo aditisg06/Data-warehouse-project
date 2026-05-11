68f61cb79593459d8dd06f7d13742c9f8c025b7b
aditisg06 <124760046+aditisg06@users.noreply.github.com>
2026-05-11 18:46:53 +0530

    Update logger.py

--- python/logger.py
+++ python/logger.py
@@ -1,1 +1,25 @@
+import logging
+import os
+
+def get_logger(name):
+    os.makedirs("../logs", exist_ok=True)
+    
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    
+    file_handler = logging.FileHandler("../logs/pipeline.log")
+    file_handler.setFormatter(formatter)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    return logger
 
